7adda99c972b3d681f250cd94378c42d599e0250
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-01-16 17:31:22 -0800

    feat: implement sync service for API requests and update route imports

--- ui/src/routes/api/radicale-users.$username.backfill.ts
+++ ui/src/routes/api/radicale-users.$username.backfill.ts
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { json } from '@tanstack/react-start'
-import { proxyRequest } from './_proxy'
+import { proxyRequest } from '../../lib/sync-service'
 
 export const Route = createFileRoute('/api/radicale-users/$username/backfill')({
 	server: {
