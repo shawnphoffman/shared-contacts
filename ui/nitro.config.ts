ac7fc595777e2f9416673559bdd19fda598e054e
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-01-16 16:12:58 -0800

    chore: update docker-compose files to improve service configuration and add RADICALE_URL environment variable

--- ui/nitro.config.ts
+++ ui/nitro.config.ts
@@ -1,3 +1,5 @@
+const RADICALE_URL = process.env.RADICALE_URL || 'http://radicale:5232'
+
 export default {
   devServer: {
     port: 3030,
@@ -5,4 +7,12 @@ export default {
   },
   port: 3030,
   host: '0.0.0.0',
+  routeRules: {
+    '/carddav': {
+      proxy: `${RADICALE_URL}/`,
+    },
+    '/carddav/**': {
+      proxy: `${RADICALE_URL}/**`,
+    },
+  },
 }
