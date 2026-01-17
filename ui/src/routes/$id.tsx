f0608fb94301ef784c77c4d1ab96d7dea70e674a
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-01-16 17:51:47 -0800

    refactor: remove optional chaining in character extraction logic for consistency and clarity

--- ui/src/routes/$id.tsx
+++ ui/src/routes/$id.tsx
@@ -135,7 +135,7 @@ function ContactDetailPage() {
 		.split(' ')
 		.filter(Boolean)
 		.slice(0, 2)
-		.map(part => part[0]?.toUpperCase())
+		.map(part => part[0].toUpperCase())
 		.join('')
 
 	const fallbackStructuredAddress = {
